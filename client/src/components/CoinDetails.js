b6e97f107ba4a6037aa72ef5b03299d13b8124c7
Erin Shields <elshields92@gmail.com>
2020-10-28 20:21:21 -0300

    update styling of coin details page

--- client/src/components/CoinDetails.js
+++ client/src/components/CoinDetails.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { actions, thunks } from '../store/coin';
 import ChartComponent from './stockchartComponents/ChartComponent'
-import { Grid } from '@material-ui/core';
+import { Button, Divider, Grid, Paper, Typography } from '@material-ui/core';
 import useStyles from '../styles.js';
 
 function CoinDetails(props) {
@@ -38,48 +38,77 @@ function CoinDetails(props) {
     const displayChangeData = (priceChange, percentChange) => {
         if (priceChange.usd) {
             if (priceChange.usd > 0) {
-                return <li>+${priceChange.usd.toFixed(2)} (+{percentChange.usd.toFixed(2)}%) Today</li>
+                return <>
+                    <Typography variant="subtitle2">+${priceChange.usd.toFixed(2)} (+{percentChange.usd.toFixed(2)}%)  <span style={{ fontWeight: "lighter" }}>Today</span></Typography>
+                </>
             } else {
-                return <li>-${Math.abs(priceChange.usd).toFixed(2)} (-{Math.abs(percentChange.usd).toFixed(2)}%) Today</li>
+                return <>
+                    <Typography variant="subtitle2">-${Math.abs(priceChange.usd).toFixed(2)} (-{Math.abs(percentChange.usd).toFixed(2)}%)  <span style={{ fontWeight: "lighter" }}>Today</span></Typography>
+                </>
             }
         } else return null
     }
 
     return (
+
         <div className={classes.grow} style={{
             paddingLeft: 30,
             paddingRight: 30,
-            maxWidth: '60%',
+            maxWidth: '80%',
+            marginTop: 30,
         }}>
-            <ChartComponent className='stockchart' />
+
+
             <Grid
                 container
                 direction="row"
-                justify="center"
+                justify="space-around"
                 alignItems="flex-start"
-                spacing={3}
+                spacing={6}
             >
-                <Grid item xs={8}></Grid>
-                <Grid item xs={4}></Grid>
+                <Grid item xs={8}>
+                    {
+                        name ?
+                            <div>
+                                <Typography variant="h5">
+                                    {name}
+                                </Typography>
+                                {current_price_usd.usd ?
+                                    <Typography variant="h5">
+                                        ${current_price_usd.usd.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
+                                    </Typography>
+                                    : null}
+                                {displayChangeData(price_change_usd, percent_change_usd)}
+                                <ChartComponent className='stockchart' />
+                                {description !== "" ?
+                                    <div>
+                                        <Typography variant="h6">About</Typography>
+                                        <Divider style={{ marginTop: 10, marginBottom: 10 }} />
+                                        <Typography variant="caption"><span dangerouslySetInnerHTML={{ __html: description }}></span></Typography>
+                                    </div>
+                                    : null}
+                            </div>
+                            : <h2>Loading...</h2>}
+                </Grid>
+                <Grid item xs={4} style={{ textAlign: 'center' }}>
+                    {name ?
+                        <>
+                            <Paper elevation={3} style={{ textAlign: 'center', padding: 10 }}>
+                                <Typography variant="subtitle2">Buy {symbol.toUpperCase()}</Typography>
+                                <Divider style={{ marginTop: 10, marginBottom: 10 }} />
+                                <div style={{ height: 200 }}>
+                                    <Typography variant="subtitle2">Placeholder for Simulation Functionality</Typography>
+                                </div>
+                            </Paper>
+                            <Button variant="outlined" color="primary" style={{ margin: 20 }}>
+                                &#10003; Add to List
+                        </Button>
+                        </>
+                        : null}
+                </Grid>
             </Grid>
-            <h2>{coinId}</h2>
-            {name ?
-                <ul>
-                    <li>Name: {name}</li>
-                    <li>Symbol: {symbol}</li>
-                    {description !== "" ?
-                        <li>Description: <span dangerouslySetInnerHTML={{ __html: description }} ></span></li>
-                        : null}
-                    {current_price_usd.usd ?
-                        <li>Current price: ${current_price_usd.usd.toFixed(3)}</li>
-                        : null}
-                    {displayChangeData(price_change_usd, percent_change_usd)}
-                    {chart_data.length > 0 ?
-                        <li className="5">{chart_data}</li>
-                        : null}
-                </ul>
-                : <h2>Loading...</h2>}
-        </div>
+
+        </div >
     );
 }
 
--- client/src/components/stockchartComponents/ChartResizer.js
+++ client/src/components/stockchartComponents/ChartResizer.js
@@ -3,10 +3,10 @@ import useDimensions from 'react-use-dimensions'
 import Chart from './Chart'
 
 //wrapper is needed to resize (add dimensions) to the chart
-function ChartWrapper (props) {
+function ChartWrapper(props) {
   const [ref, { width, height }] = useDimensions()
   return (
-    <div ref={ref} style={{ width: '50%', height: '50%' }}>
+    <div ref={ref} style={{ width: '100%', height: '100%' }}>
       <Chart
         {...props}
         height={height}
