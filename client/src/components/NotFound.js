7fd3dec014d5dbf9b9ea8e851c4bb857c973630b
Daniel Ford <daniel.fordd@gmail.com>
2020-10-28 22:33:37 -0400

    404 route

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFound = () => {
+
+return(
+    <>
+    <img src="/doge.png" />
+    <span >404, Nothing to see here</span>
+    <img src="/leftdoge.png" />
+    </>
+)
+}
+
+export default NotFound
