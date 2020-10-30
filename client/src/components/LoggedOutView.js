8ad45ae70c4c28da4f6b8604e82c350fbefc372c
Daniel Ford <daniel.fordd@gmail.com>
2020-10-30 11:58:49 -0400

    merging still

--- client/src/components/LoggedOutView.js
+++ client/src/components/LoggedOutView.js
@@ -8,20 +8,18 @@ const LoggedOutView = () => {
 return(
 
 <BrowserRouter>
-    <Switch>
-        <Route path="/signup">
-            <Container maxWidth="md" style={{ marginTop: 40 }}>
-                <SignUpForm />
-            </Container>
-        </Route>
-        <Route exact path="*">
-            <Container maxWidth="md" style={{ marginTop: 40 }}>
-                <LoginForm />
-                <h2>Explore the world of crypto currencies.</h2>
-                <p>Everyone has heard of bitcoin, but did you know there are so many more? Sign up to explore all of the crypto currencies out there! You might even find some suprising ones like <a href="https://dogecoin.com/">Dogecoin</a>.</p>
-            </Container>
-        </Route>
-    </Switch>
+    <Container maxWidth="md" style={{ marginTop: 40 }}>
+        <Switch>
+            <Route path="/signup">
+                    <SignUpForm />
+            </Route>
+            <Route exact path="*">
+                    <LoginForm />
+                    <h2>Explore the world of crypto currencies.</h2>
+                    <p>Everyone has heard of bitcoin, but did you know there are so many more? Sign up to explore all of the crypto currencies out there! You might even find some suprising ones like <a href="https://dogecoin.com/">Dogecoin</a>.</p>
+            </Route>
+        </Switch>
+    </Container>
 </BrowserRouter>
 )
 }
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
-<<<<<<< HEAD
-import { NavLink, Redirect } from 'react-router-dom';
-=======
 import { NavLink, useHistory } from 'react-router-dom';
->>>>>>> 8f53bff1c231929814451a766bd1fd62badb39cc
 import * as AuthAction from '../store/session';
 import { Button, Grid, Paper, TextField, Typography } from '@material-ui/core';
 import useStyles from '../styles.js';
@@ -18,51 +14,6 @@ const LoginForm = () => {
   const token = useSelector(state => state.session.id);
   const history = useHistory();
 
-<<<<<<< HEAD
-const LoginForm = () =>{
-    const dispatch = useDispatch()
-    const login = (email, password) => dispatch(AuthAction.login(email, password))
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const token = useSelector(state => state.session.id);
-
-    if (token) {
-        return null
-    }
-
-    const updateEmail = (e) => {
-     setEmail(e.target.value)
-   }
-
-   const updatePassword = (e) => {
-     setPassword(e.target.value)
-   }
-
-   const loginHandler = e => {
-     e.preventDefault()
-     login(email, password)
-     return <Redirect to="/" />
-   }
-
-    return (
-      <form onSubmit={loginHandler}>
-        <input type="text"
-              placeholder="Email"
-              value={email}
-              onChange={updateEmail}
-              />
-        <input type="password"
-              placeholder="Password"
-              value={password}
-              onChange={updatePassword}
-              />
-        <button type="submit">Login</button>
-        <div className="signup-form-login">
-            <div>Dont have an account?</div><NavLink to="/signup"> Sign Up</NavLink>
-        </div>
-      </form>
-      );
-=======
   if (token) {
     return null
   }
@@ -117,7 +68,6 @@ const LoginForm = () =>{
       </Grid>
     </Grid>
   );
->>>>>>> 8f53bff1c231929814451a766bd1fd62badb39cc
 }
 
 export default LoginForm
