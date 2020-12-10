8b829384dfd696fe41f762e4759e64c3e4ecd78b
johnpatrickanders <johnpatrickanders@gmail.com>
2020-12-10 14:18:37 -0500

    fix no lost v1

--- client/src/components/CoinModal.js
+++ client/src/components/CoinModal.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Button, Divider, Modal, Toolbar, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import AddListItem from './AddListItem'
+import AddListItem from './AddListItem';
+import { thunks } from '../store/list';
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
@@ -32,12 +33,19 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SimpleModal() {
   const classes = useStyles();
-  const userLists = useSelector(state => state.list.lists)
+  const dispatch = useDispatch();
+  let userLists = useSelector(state => state.list.lists)
+  const userId = useSelector(state => state.session.id)
   // getModalStyle is not a pure function, we roll the style only on the first render
   const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = React.useState(false);
 
   const handleOpen = () => {
+    if (userLists.length === 0) {
+      console.log('ZERO LISTS')
+      dispatch(thunks.createNewList(userId, 'Watch List'));
+      userLists = thunks.getAllUserLists(userId);
+    }
     setOpen(true);
   };
 
@@ -49,12 +57,12 @@ export default function SimpleModal() {
     <div style={modalStyle} className={classes.paper}>
       <Typography variant="h4" id="simple-modal-title">Lists</Typography>
       {userLists.map(list => <Typography key={`${list[0]}-${list[1]}`}>
-                              <Toolbar>
-                                {list[0]}
-                                <AddListItem listTitle={list[0]} listId={list[1]} />
-                                </Toolbar>
-                              <Divider style={{ marginTop: 10, marginBottom: 10 }} />
-                            </Typography>)}
+        <Toolbar>
+          {list[0]}
+          <AddListItem listTitle={list[0]} listId={list[1]} />
+        </Toolbar>
+        <Divider style={{ marginTop: 10, marginBottom: 10 }} />
+      </Typography>)}
     </div>
   );
 
